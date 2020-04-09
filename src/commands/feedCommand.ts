669c52aeeb7c1bc8d52b901b11f339f3fbe9c51b
Hock Shem <hockshem.c@gmail.com>
2020-04-09 17:39:25 +0800

    Used formatted local time in covid feed report

--- src/commands/feedCommand.ts
+++ src/commands/feedCommand.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 import CommandContext from "../models/command_context";
 import Command from "./command";
 import Axios from "axios";
+import { DateTime, FixedOffsetZone } from "luxon";
 import { validateEnvEntry } from "../utils/validator";
 import CovidFeed from "../models/serializable_models/covid_feed";
 import Discord = require('discord.js');
@@ -23,7 +24,6 @@ export default class CovidFeedCommand implements Command {
     async execute(context: CommandContext): Promise<void> {
         if (context.arguments.length == 0) {
             await context.originalMessage.reply(this.helpMessage(context.prefix));
-            // TODO: Checkout on Promise Reject & Resolve.
         }
 
         const targetUrl: string = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php";
@@ -44,6 +44,12 @@ export default class CovidFeedCommand implements Command {
             .then((feed) => {
                 const thumbnailUrl = "https://s3-eu-west-2.amazonaws.com/north-live/wp-content/uploads/2020/03/05163005/Coronavirus-Thumbnail-Image.jpg";
 
+                const rawDateTime = feed.recordDate || "-";
+                const formattedLocalDateTime = DateTime.fromSQL(rawDateTime)
+                    .plus({ hours: 8 })
+                    .setLocale('en-US')
+                    .toLocaleString({ weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' });
+
                 const feedEmbed = new Discord.MessageEmbed()
                     .setColor('#008080')
                     .setTitle(`${feed.countryName}'s COVID-19 Updates`)
@@ -59,7 +65,7 @@ export default class CovidFeedCommand implements Command {
                         { name: "Serious Critical", value: `${feed.seriousCritical}`, inline: true },
                     )
                     .setThumbnail(thumbnailUrl)
-                    .setFooter(`${feed.recordDate}`)
+                    .setFooter(`Last updated on: ${formattedLocalDateTime}`)
                 return feedEmbed;
             })
             .then((feedEmbed) => context.originalMessage.reply(feedEmbed))
